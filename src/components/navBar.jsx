0e5d4d582639eb3a2b9074d7747a4b6dfceab692
adelin-diac <105500899+adelin-diac@users.noreply.github.com>
2022-07-24 19:01:31 +0100

    updating everything

---
+++ src/components/navBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div id="navContainer">
+        <p>This will be a nav bar - add bootstrap to this app</p>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
