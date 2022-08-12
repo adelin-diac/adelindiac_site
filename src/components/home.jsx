e844ed8896ba74acb7ab55156d1f787ea6937e31
adelin-diac <105500899+adelin-diac@users.noreply.github.com>
2022-08-12 18:38:46 +0100

    fixed mobile veiw of home section

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,91 +1,112 @@
-import React, { Component } from "react";
+import React from "react";
+import "../styles/home.css";
 
-class HomeSection extends Component {
-  render() {
-    return (
-      <div id="home" className="float-container" style={floatContainerStyle}>
-        <div id="social-links" style={socialLinksStyle}>
+const HomeSection = () => {
+  return (
+    <div
+      id="home"
+      className="float-container"
+      style={styles.floatContainerStyle}
+    >
+      <div id="row" style={styles.rowDivStyle}>
+        <div id="social-links" style={styles.socialLinksStyle}>
           <a
             href="https://www.github.com/adelin-diac"
             target="_blank"
-            style={socialImgStyles}
+            rel="noreferrer"
+            style={styles.socialImgStyles}
           >
             <img
               alt="GitHub Logo"
               src="./images/github.png"
-              style={socialImgStyles}
+              style={styles.socialImgStyles}
             ></img>
           </a>
           <a
             href="https://www.linkedin.com/in/adelin-denis-diac/"
             target="_blank"
-            style={socialImgStyles}
+            rel="noreferrer"
+            style={styles.socialImgStyles}
           >
             <img
               alt="LinkedIn Logo"
               src="./images/linkedin.png"
-              style={socialImgStyles}
+              style={styles.socialImgStyles}
             ></img>
           </a>
-          <a href="" style={socialImgStyles}>
+          <a href="#" style={styles.socialImgStyles}>
             <img
               alt="Email Logo"
               src="./images/email.png"
-              style={socialImgStyles}
+              style={styles.socialImgStyles}
             ></img>
           </a>
         </div>
         <div
           id="homeImageDiv"
           className="float-child"
-          style={homeImageDivStyle}
+          style={styles.homeImageDivStyle}
         >
           <img
             id="home-img"
-            style={homeImgStyle}
+            style={styles.homeImgStyle}
             src="./images/adelindiac-img.png"
-            alt="Adelin Diac Profile Picture"
+            alt="Adelin Diac"
           ></img>
         </div>
-        <div id="home-content" className="float-child" style={homeContentStyle}>
-          <h3 className="dark">Adelin Diac</h3>
-          <p>Hello</p>
-          <p className="lead">
-            Have a section on Mathshelp ireland and link info about it there
-            Make the email link to a "contact" section that emails you
-          </p>
-        </div>
       </div>
-    );
-  }
-}
-const floatContainerStyle = {
-  padding: "10px",
-  color: "white",
-  display: "flex",
-  flexDirection: "row",
-  height: "270px",
+      <div
+        id="home-content"
+        className="float-child"
+        style={styles.homeContentStyle}
+      >
+        <h3 className="dark">Adelin Diac</h3>
+        <p>Software Engineer & Entrepreneur</p>
+        <p className="lead">
+          Have a section on Mathshelp ireland and link info about it there Make
+          the email link to a "contact" section that emails you
+        </p>
+      </div>
+    </div>
+  );
 };
-const homeImageDivStyle = {
-  width: "40%",
-};
-const homeImgStyle = {
-  width: "250px",
-};
-const socialLinksStyle = {
-  marginTop: "65px",
-  width: "10%",
-  display: "flex",
-  flexDirection: "column",
-};
-const socialImgStyles = {
-  width: "30px",
-  height: "30px",
-  margin: "0 auto",
-  marginBottom: "10px",
-};
-const homeContentStyle = {
-  width: "50%",
+const styles = {
+  floatContainerStyle: {
+    padding: "10px",
+    color: "white",
+    display: "flex",
+    flexDirection: "row",
+    // height: "270px",
+  },
+  rowDivStyle: {
+    display: "flex",
+    flexDirection: "row",
+    width: "80%",
+  },
+  socialLinksStyle: {
+    marginTop: "65px",
+    width: "20%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  homeImageDivStyle: {
+    width: "80%",
+  },
+  homeImgStyle: {
+    width: "250px",
+  },
+  socialImgStyles: {
+    width: "30px",
+    height: "30px",
+    margin: "0 auto",
+    marginBottom: "10px",
+  },
+  homeContentStyle: {
+    width: "90%",
+    marginTop: "30px",
+    textAlign: "center",
+    marginRight: "50px",
+  },
 };
 
 //THIS IS THE INSTAGRAM IMAGE WITH LINK TO MHI INSTA
@@ -96,9 +117,9 @@ target="_blank"
 style={socialImgStyles}
 >
 <img
-  alt="Instagram Logo"
-  src="./images/instagram.png"
-  style={socialImgStyles}
+	alt="Instagram Logo"
+	src="./images/instagram.png"
+	style={socialImgStyles}
 ></img>
 </a> */
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,59 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    const navbarNavStyle = {
+const NavBar = () => {
+  const styles = {
+    navbarNavStyle: {
       marginLeft: "auto",
-    };
-    return (
-      <div className="container">
-        <nav
-          className="navbar navbar-expand-md navbar-dark"
-          style={{ backgroundColor: "#161718" }}
+    },
+  };
+  return (
+    <div className="container">
+      <nav
+        className="navbar navbar-expand-md navbar-dark"
+        style={{ backgroundColor: "#161718" }}
+      >
+        <a className="navbar-brand" href="#home">
+          Adelin Diac
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <a className="navbar-brand" href="#home">
-            Adelin Diac
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav" style={navbarNavStyle}>
-              <li className="nav-item ">
-                <a href="#home" className="nav-link active">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  Projects
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  MathsHelpIreland
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav" style={styles.navbarNavStyle}>
+            <li className="nav-item ">
+              <a href="#home" className="nav-link active">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                MathsHelpIreland
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
