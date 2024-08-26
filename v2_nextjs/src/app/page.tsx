cb07d5a1fa9cb3ac36ef4dc0dcf9dbdc4a3bd6ec
adelin-diac <adelin334@gmail.com>
2024-08-26 12:40:38 +0100

    Update page.tsx

--- v2_nextjs/src/app/page.tsx
+++ v2_nextjs/src/app/page.tsx
@@ -1,4 +1,5 @@
 export default function Home() {
+  console.log("The home page is fetched...");
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <p>Hello. This is the new version of my website.</p>
