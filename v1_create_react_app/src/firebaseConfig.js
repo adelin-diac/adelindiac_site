e0d5c3ba47fbb38e79b4ca3319f46f5433881ddc
adelin-diac <adelin334@gmail.com>
2024-08-27 13:41:51 +0100

    add firebase config to v1

--- v1_create_react_app/src/firebaseConfig.js
+++ v1_create_react_app/src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "REMOVED",
-  authDomain: "REMOVED",
-  projectId: "REMOVED",
-  storageBucket: "REMOVED",
-  messagingSenderId: "REMOVED",
-  appId: "REMOVED",
+  apiKey: process.env.V1_APIKEY,
+  authDomain: process.env.V1_AUTH_DOMAIN,
+  projectId: process.env.V1_PROJECT_ID,
+  storageBucket: process.env.V1_STORAGE_BUCKET,
+  messagingSenderId: process.env.V1_MESSAGING_SENDER_ID,
+  appId: process.env.V1_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
